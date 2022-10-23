89ed40006e88f8bf2425ac9c2e680a173ba9cbe2
tdubois <tdubois@student.42angouleme.fr>
2022-10-23 20:20:44 +0200

    setup nvim lsp keymap and snippet engine

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -144,6 +144,11 @@ require('nvim-treesitter.configs').setup({
 local cmp = require('cmp');
 
 cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+		end
+	},
 	mapping = cmp.mapping.preset.insert({
 		['<c-space>'] = cmp.mapping.complete(),
 		['<c-e>'] = cmp.mapping.abort(),
@@ -187,13 +192,35 @@ cmp.setup.cmdline(':', {
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities();
 
+local opts = { noremap=true, silent=true };
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts);
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc');
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr };
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts);
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts);
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts);
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts);
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts);
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts);
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts);
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts);
+end
 -- #############################################################################
 -- ## p00f/clangd_extensions.nvim                                              #
 -- #############################################################################
 
 require('clangd_extensions').setup({
 	server = {
-		capabilities = capabilities
+		capabilities = capabilities,
+		on_attach = on_attach
 	}
 });
 
--- home/private_dot_config/nvim/lua/plugins.lua
+++ home/private_dot_config/nvim/lua/plugins.lua
@@ -55,9 +55,11 @@ return require('packer').startup({
 				'hrsh7th/cmp-buffer',
 				'hrsh7th/cmp-path',
 				'hrsh7th/cmp-cmdline',
-				'hrsh7th/cmp-nvim-lua'
+				'hrsh7th/cmp-nvim-lua',
+				'L3MON4D3/LuaSnip',
+				'saadparwaiz1/cmp_luasnip'
 			}
-		})
+		});
 
 		-- Automatically set up your configuration after cloning packer.nvim
 		-- Put this at the end after all plugins
