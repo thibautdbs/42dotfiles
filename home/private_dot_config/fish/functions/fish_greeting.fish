fea3486aa0a6138b442d7fa0d85bb4945793bceb
tdubois <tdubois@student.42angouleme.fr>
2022-10-24 16:34:57 +0200

    changed motd from txt file to bash script

--- home/private_dot_config/fish/functions/fish_greeting.fish
+++ home/private_dot_config/fish/functions/fish_greeting.fish
@@ -1,8 +1,8 @@
 function fish_greeting;
 	if not set -q MOTDSHOWN;
-		set motd "$HOME/.config/fish/motd";
+		set motd "$HOME/.config/fish/motd.bash";
 		if test -f $motd;
-			cat $motd;
+			cat $motd | bash;
 		end
 		set 42dfmsg "$(42df fetch)";
 		if not test -z $42dfmsg;
