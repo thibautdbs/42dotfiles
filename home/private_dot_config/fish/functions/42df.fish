105041fd3b3401e06f6e09ab07b9a3b7bdcc7807
tdubois <tdubois@student.42angouleme.fr>
2022-10-24 13:38:33 +0200

    updated 42df script

--- home/private_dot_config/fish/functions/42df.fish
+++ home/private_dot_config/fish/functions/42df.fish
@@ -1,6 +1,17 @@
-function 42df																\
---wraps="chezmoi --source $HOME/.local/share/42dotfiles"					\
---description "alias 42df chezmoi --source $HOME/.local/share/42dotfiles"	
-	chezmoi --source "$HOME/.local/share/42dotfiles" $argv
-   
+function 42df
+	set chezmoiflags --source "$HOME/.local/share/42dotfiles/";
+	if test (count $argv) -eq 0;
+		return ;
+	end
+	if test $argv[1] = 'fetch';
+		set gitcmd status --short --branch --porcelain=2;
+		if test -z (chezmoi $chezmoiflags git -- $gitcmd | grep 'branch.ab' | grep '\-0');
+			echo '42df update available, please run `42df update`.';
+		else
+			echo '42df is up to date.';
+		end
+	end
+	if test $argv[1] = 'update';
+		chezmoi $chezmoiflags update
+	end
 end
