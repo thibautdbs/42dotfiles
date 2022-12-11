8a90475881c66a9dfbade3da4e09c252b2d29b82
tdubois <tdubois@student.42angouleme.fr>
2022-12-11 20:04:35 +0100

    fixing packersync

--- home/private_dot_config/fish/functions/packersync.fish
+++
@@ -1,4 +0,0 @@
-function packersync --description 'alias packersync=nvim -u NORC --headless -c "autocmd User PackerComplete quitall" -c "lua require([[bootstrap]])"'
-  nvim -u NORC --headless -c 'autocmd User PackerComplete quitall' -c 'lua require([[bootstrap]])' $argv
-        
-end
--- home/private_dot_config/fish/functions/run_after_install_compile_db.sh
+++ home/private_dot_config/fish/functions/run_after_install_compile_db.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo "updating cdb...";
 yes | pip install --user --upgrade compiledb;
