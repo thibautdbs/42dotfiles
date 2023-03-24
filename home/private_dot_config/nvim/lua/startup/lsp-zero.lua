ef2e86217cbae78eef12cb2f77beed4295dd4f4f
tdubois <tdubois@student.42angouleme.fr>
2023-03-24 01:06:01 +0100

    minor fixes on nvim config and mk script

--- home/private_dot_config/nvim/lua/startup/lsp-zero.lua
+++ home/private_dot_config/nvim/lua/startup/lsp-zero.lua
@@ -1,51 +1,82 @@
 return(function(use)
 
 use({
-	'VonHeikemen/lsp-zero.nvim',
-	requires = {
-		-- LSP Support
-		'neovim/nvim-lspconfig',
-		'williamboman/mason.nvim',
-		'williamboman/mason-lspconfig.nvim',
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+        -- Lib
+        'nvim-lua/plenary.nvim',
 
-		-- Autocompletion
-		'hrsh7th/nvim-cmp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'saadparwaiz1/cmp_luasnip',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-nvim-lua',
+        -- LSP Support
+        'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
 
-		-- Snippets
-		'L3MON4D3/LuaSnip'
-	},
-	config = function ()
-		local lsp = require('lsp-zero');
-		lsp.preset('recommended');
+        -- Formatters
+        'jose-elias-alvarez/null-ls.nvim',
+        'jayp0521/mason-null-ls.nvim',
 
-		lsp.ensure_installed({
-			'clangd'
-		});
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
 
-		local cmp = require('cmp');
-		lsp.setup_nvim_cmp({
-			mapping = cmp.mapping.preset.insert({
-				['<c-space>'] = cmp.mapping.complete(),
-				['<c-e>'] = cmp.mapping.abort(),
-				['<cr>'] = cmp.mapping.confirm({ select = false })
-			})
-		});
+        -- Snippets
+        'L3MON4D3/LuaSnip'
+    },
+    config = function ()
+        local lsp = require('lsp-zero').preset('recommended');
 
-		lsp.nvim_workspace({
-			library = vim.api.nvim_get_runtime_file('', true)
-		});
+        -- COMPLETIONS
+        local cmp = require('cmp');
+        lsp.setup_nvim_cmp({
+            mapping = cmp.mapping.preset.insert({
+                ['<c-space>'] = cmp.mapping.complete(),
+                ['<c-e>'] = cmp.mapping.abort(),
+                ['<cr>'] = cmp.mapping.confirm({ select = false })
+            })
+        });
 
-		lsp.setup();
+        lsp.nvim_workspace({
+            library = vim.api.nvim_get_runtime_file('', true)
+        });
 
-		vim.diagnostic.config({
-			virtual_text = true
-		});
-	end
+        lsp.setup();
+
+        -- null_ls: admit non-lsp sources
+        local null_ls = require("null-ls");
+        local null_opts = lsp.build_options('null-ls', {});
+
+        null_ls.setup({
+            on_attach = function(client, bufnr)
+                null_opts.on_attach(client, bufnr);
+
+                local opts = {buffer = bufnr}
+                local bind = vim.keymap.set
+
+                -- use <leader>f to format buffer.
+                local cmd = '<cmd>lua vim.lsp.buf.format()<cr>';
+                bind('n', '<leader>f', cmd, opts);
+                -- more keybindings...
+            end
+        });
+
+        -- Allow null-ls sources to be installed with Mason
+        require("mason-null-ls").setup({
+            ensure_installed = nil,
+            automatic_installation = true,
+            automatic_setup = true
+        });
+
+        -- Required when `automatic_setup` is true
+        require("mason-null-ls").setup_handlers({});
+
+        vim.diagnostic.config({
+            virtual_text = true
+        });
+    end
 });
 
 end);
